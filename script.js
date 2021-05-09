c4712d1fa89eb7246d8608fb9e18db132b60ac60
ant <nivaniucc@gmail.com>
2021-05-09 17:52:31 -0400

    Changed up the functions for password generation. I have yet to add an alert for if the user wants only uppercase or lowercase. I'll try this next

--- script.js
+++ script.js
@@ -26,76 +26,37 @@ function checkLengthValidation(parselength) {
   var parselength = parseInt(length);
   if (parselength < 8 || parselength > 128) {
     alert("Enter a valid number between 8 to 128")
-  }
+    return "This input is invalid."
+  } 
 
   else if (isNaN(parselength)) {
     alert("Length can not be string");
+    return "This input is invalid."
 
   }
   else {
-   return lowerCase(parselength);
+
+    var numConfirmBox = confirm("Do you want to include numbers?");
+    var symConfirmBox = confirm("Do you want to include special characters?");
+    var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var num = '0123456789';
+    var sym = '!@#$%^&*=-_';
+
+      var characters = char;
+      (numConfirmBox) ? characters += num : '';
+      (symConfirmBox) ? characters += sym : '';
+      return password(parselength, characters);
   }
 }
 
+function password(l, characters) {
+  console.log("Inside Function : " + l);
+  var pwd = '';
+  for (var i = 0; i < l; i++) {
+    console.log("Inside Function before loop pwd is:" + pwd);
+    pwd = pwd + characters.charAt(Math.floor(Math.random() * characters.length));
+    console.log("Inside Function loop pwd is:" + pwd);
 
-function lowerCase(l) {
-
-  console.log("value of l is " + l)
-  var lowerCases = confirm("Do you want to include lower case")
-
-  if (lowerCases === false) {
-    upperCase(lowerCases);
   }
-  else {
-      //Lowercase character codes start from 97 to 122...I think! 
-      lowerCases = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-      console.log("lower caseS : " + lowerCases);
-      return upperCase(lowerCases);
-  }
-}
-
-
-function upperCase(l) {
-
-  var upperCases = confirm("Do you want to include Upper case")
-
-  if (upperCases === false) {
-    number(upperCases);
-  }
-  else {
-    upperCases = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-    upperCases += l;
-    return number(upperCases);
-  }
-
-}
-
-function number(l) {
-
-  var numbers = confirm("Do you want to include Numbers ")
-
-  if (numbers === false) {
-    symbol(numbers);
-  }
-  else {
-    numbers = String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-    numbers += l;
-    return symbol(numbers);
-  }
-}
-
-
-function symbol(l) {
-
-  var symbols = confirm("Do you want to include special characters ")
-
-  if (symbols === false) {
-    checkLengthValidation();
-  }
-  else {
-    const specialCharacters = "!@#$%^&*()_+<>,./"
-    symbols = specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-    symbols += l;
-    return symbols;
-  }
+  return pwd;
 }
